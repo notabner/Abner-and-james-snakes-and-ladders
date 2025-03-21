ae53af4587696da8b5c07cc484b4f5c3dd3e29e1
notabner <abneryang.sg@hotmail.com>
2025-03-21 15:53:57 +0000

    Update dees_aaaaaaaaaaaaa.py

--- dees_aaaaaaaaaaaaa.py
+++ dees_aaaaaaaaaaaaa.py
@@ -1,3 +1,6 @@
+import random
+import os
+
 p1d = 0
 p2d = 0
 p1 = 0
@@ -17,61 +20,80 @@ y9 = ["100","99","98","97","96","95","94","93","92","91"]
 
 ylist = [y9,y8,y7,y6,y5,y4,y3,y2,y1,y0]
 
-def dice_rollp1(p1d):
-    p1d = random.randint(1, 6)
+# Define snakes and ladders
+snakes = [random.randint(1, 100) for _ in range(5)]  # List of snake head positions
+ladders = {20: 40, 35: 70, 50: 80, 60: 90, 80: 100}  # Ladder positions and where they take you
+
+def dice_rollp1(p1):
+    p1 = random.randint(1, 6)
     return p1
 
-def dice_rollp2(p2d):
-    p2d = random.randint(1, 6)
+def dice_rollp2(p2):
+    p2 = random.randint(1, 6)
     return p2
+
 def findPlayerPos(y):
-  if int(y) == p1:
-    return "P1"
-  elif int(y) == p2:
-    return "P2"
-  else:
-    return y
-def printTable():
-  for i in ylist:
-    if not i == y9:
-      printY = " "
+    if int(y) == p1:
+        return "P1"
+    elif int(y) == p2:
+        return "P2"
     else:
-      printY = ""
-    
-    for y in i:
-      plrPos = findPlayerPos(y)
-      printY += plrPos
-      printY += "|  "
-    print(printY)
+        return y
+
+def printTable():
+    for i in ylist:
+        if not i == y9:
+            printY = " "
+        else:
+            printY = ""
+        
+        for y in i:
+            plrPos = findPlayerPos(y)
+            printY += plrPos
+            printY += "|  "
+        print(printY)
+
+# Main game loop
 while True:
- 
-    snakes = random.randint(1, 100)
-    snake1 = random.randint(1, 100)
-    snake2 = random.randint(1, 100)
-    snake3 = random.randint(1, 100)
-    snake4 = random.randint(1, 100)
-    
-    print(snakes, snake1, snake2, snake3, snake4)
+    print("Snakes positions:", snakes)
+    print("Ladders positions:", ladders)
 
     if turn == 0:
         p1d = dice_rollp1(p1d)
         turn = turn + 1
-        print("player 1 rolled", p1d)
+        print("Player 1 rolled", p1d)
         p1 = p1 + p1d
-        if p1 == snakes or p1 == snake1 or p1 == snake2 or p1 == snake3 or p1 == snake4:
+        
+        # Check for snakes
+        if p1 in snakes:
             p1 = p1 - random.randint(1, 10)
-            print("haha loser u hit a snake, p1 is now on", p1)
-        print("player 1 is on", p1)
+            print("Haha loser, you hit a snake! P1 is now on", p1)
+        
+        # Check for ladders
+        if p1 in ladders:
+            p1 = ladders[p1]
+            print("You climbed a ladder! P1 is now on", p1)
+        
+        print("Player 1 is on", p1)
+    
     elif turn == 1:
         p2d = dice_rollp2(p2d)
         turn = turn - 1
-        print("player 2 rolled", p2d)
+        print("Player 2 rolled", p2d)
         p2 = p2 + p2d
-        if p2 == snakes or p2 == snake1 or p2 == snake2 or p2 == snake3 or p2 == snake4:
+        
+        # Check for snakes
+        if p2 in snakes:
             p2 = p2 - random.randint(1, 10)
-            print("haha loser u hit a snake, p2 is now on", p2)
-        print("player 2 is on", p2)
+            print("Haha loser, you hit a snake! P2 is now on", p2)
+        
+        # Check for ladders
+        if p2 in ladders:
+            p2 = ladders[p2]
+            print("You climbed a ladder! P2 is now on", p2)
+        
+        print("Player 2 is on", p2)
 
-    input("press enter to roll")
+    input("Press enter to roll")
     os.system('clear')
     printTable()
