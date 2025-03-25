e4d21fbfcef7ada82267bccba28ad5cdcee9cd36
notabner <abneryang.sg@hotmail.com>
2025-03-25 12:15:25 +0000

    Update dees_aaaaaaaaaaaaa.py

--- dees_aaaaaaaaaaaaa.py
+++ dees_aaaaaaaaaaaaa.py
@@ -21,8 +21,8 @@ y9 = ["100","99","98","97","96","95","94","93","92","91"]
 ylist = [y9,y8,y7,y6,y5,y4,y3,y2,y1,y0]
 
 # Define snakes and ladders
-snakes = [random.randint(1, 100) for _ in range(5)]  # List of snake head positions
-ladders = {20: 40, 35: 70, 50: 80, 60: 90, 80: 100}  # Ladder positions and where they take you
+snakes = [random.randint(1, 100) for _ in range(5)]  # List of snake
+ladders = [random.randint(1, 100) for _ in range(6)]   # Ladder positions and where they take you
 
 def dice_rollp1(p1):
     p1 = random.randint(1, 6)
@@ -87,7 +87,7 @@ while True:
       turn = turn - 1
       print("Player 2 rolled", p2d)
       p2 = p2 + p2d
-      print("player2 is now on", p2)
+      print("player 2 is now on", p2)
       input("Press enter to roll")
   # Check for snakes
       if p2 in snakes:
@@ -101,5 +101,6 @@ while True:
         print("Player 2 is on", p2)
         input("Press enter to roll")
     
-    os.system('clear')
+    os.system('clear') 
     printTable()
+    
